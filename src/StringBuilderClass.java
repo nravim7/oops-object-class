adbac1929dc9ea606ff99832bd8d1c315d03230c
Ravi N <your.email@example.com>
2024-06-08 17:43:58 +0530

    StringBuilderClass

---
+++ src/StringBuilderClass.java
@@ -0,0 +1,26 @@
+import java.lang.StringBuilder;
+
+public class StringBuilderClass {
+    public static void main(String[] args) {
+
+        String helloWorld = "Hello" + " World";
+        helloWorld.concat(" and Goodbye");
+
+        StringBuilder helloWorldBuilder = new StringBuilder("Hello" + " World");
+        helloWorldBuilder.append(" and Goodbye");
+
+        printInformation(helloWorld);
+        printInformation(helloWorldBuilder);
+    }
+
+    public static void printInformation(String string) {
+        System.out.println("String = " + string);
+        System.out.println("length = " + string.length());
+    }
+
+    public static void printInformation(StringBuilder builder) {
+        System.out.println("String = " + builder);
+        System.out.println("length = " + builder.length());
+    }
+
+}
